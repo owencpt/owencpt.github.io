233e6a3d1784c67b633401a4f32545b87933b35e
owencpt <owencpt123@gmail.com>
2024-05-25 18:03:37 +1000

    added menu selections

--- script.js
+++ script.js
@@ -1,48 +1,82 @@
-// add classes for mobile navigation toggling
-var CSbody = document.querySelector("body");
-const CSnavbarMenu = document.querySelector("#cs-navigation");
-const CShamburgerMenu = document.querySelector("#cs-navigation .cs-toggle");
+document.getElementById('show-all').addEventListener('click', function() {
+    var sandwich = document.getElementById("sandwich");
+    var acai = document.getElementById("acai");
+    var smoothie = document.getElementById("smoothie");
+    var soup = document.getElementById("soup");
 
-CShamburgerMenu.addEventListener('click', function() {
-    CShamburgerMenu.classList.toggle("cs-active");
-    CSnavbarMenu.classList.toggle("cs-active");
-    CSbody.classList.toggle("cs-open");
-    // run the function to check the aria-expanded value
-    ariaExpanded();
+    if (sandwich.style.display === "none") {
+        sandwich.style.display = "inherit";
+    } 
+
+    if (acai.style.display === "none") {
+        acai.style.display = "inherit";
+    } 
+
+    if (smoothie.style.display === "none") {
+        smoothie.style.display = "inherit";
+    } 
+
+    if (soup.style.display === "none") {
+        soup.style.display = "inherit";
+    } 
+
 });
 
-// checks the value of aria expanded on the cs-ul and changes it accordingly whether it is expanded or not 
-function ariaExpanded() {
-    const csUL = document.querySelector('#cs-expanded');
-    const csExpanded = csUL.getAttribute('aria-expanded');
+document.getElementById('vegetarian').addEventListener('click', function() {
+    var soup = document.getElementById("soup");
+    var sandwich = document.getElementById("sandwich");
+    var acai = document.getElementById("acai");
+    var smoothie = document.getElementById("smoothie");
 
-    if (csExpanded === 'false') {
-        csUL.setAttribute('aria-expanded', 'true');
-    } else {
-        csUL.setAttribute('aria-expanded', 'false');
-    }
-}
+    soup.style.display = "none"
 
-    // This script adds a class to the body after scrolling 100px
-// and we used these body.scroll styles to create some on scroll 
-// animations with the navbar
 
-document.addEventListener('scroll', (e) => { 
-    const scroll = document.documentElement.scrollTop;
-    if(scroll >= 100){
-document.querySelector('body').classList.add('scroll')
-    } else {
-    document.querySelector('body').classList.remove('scroll')
-    }
+    if (sandwich.style.display === "none") {
+        sandwich.style.display = "inherit"
+    } 
+
+    if (acai.style.display === "none") {
+        acai.style.display = "inherit"
+    } 
+
+    if (smoothie.style.display === "none") {
+        smoothie.style.display = "inherit"
+    } 
 });
 
 
-// mobile nav toggle code
-const dropDowns = Array.from(document.querySelectorAll('#cs-navigation .cs-dropdown'));
-    for (const item of dropDowns) {
-        const onClick = () => {
-        item.classList.toggle('cs-active')
-    }
-    item.addEventListener('click', onClick)
-    }
-                            
+document.getElementById('vegan').addEventListener('click', function() {
+    var sandwich = document.getElementById("sandwich");
+    var acai = document.getElementById("acai");
+    var soup = document.getElementById("soup");
+    var smoothie = document.getElementById("smoothie");
+
+
+    sandwich.style.display = "none";
+    acai.style.display = "none";
+    soup.style.display = "none";
+
+    if (smoothie.style.display === "none") {
+        smoothie.style.display = "inherit"
+    } 
+
+
+});
+
+
+document.getElementById('dairy-free').addEventListener('click', function() {
+    var sandwich = document.getElementById("sandwich");
+    var acai = document.getElementById("acai");
+    var soup = document.getElementById("soup");
+    var smoothie = document.getElementById("smoothie");
+
+
+    sandwich.style.display = "none";
+    acai.style.display = "none";
+    smoothie.style.display = "none";
+
+    if (soup.style.display === "none") {
+        soup.style.display = "inherit"
+    } 
+
+});
